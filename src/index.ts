4b742873f72d55ec783a782e108d86211b65053d
Rayhan Alghiffari Azizi <rayhan.azizi9@gmail.com>
2023-09-06 21:08:38 +0700

    edit index.ts import/export syntax

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import { Diagram, polygon } from './diagram.js';
-import { V2 } from './linear_algebra.js';
-import { draw_to_svg } from './draw_svg.js';
+export { Diagram, polygon } from './diagram.js';
 
-export { Diagram, polygon }
-export { V2 }
-export { draw_to_svg }
+export { Vector2, V2 } from './linear_algebra.js';
+
+export { draw_to_svg } from './draw_svg.js';
+
+// import { rectangle, square } from './shapes.js'
+export { rectangle, square } from './shapes.js'
