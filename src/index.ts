487f5c6b2f2845757c9a5b907834f304151ac791
Rayhan Alghiffari Azizi <rayhan.azizi9@gmail.com>
2023-09-07 15:59:12 +0700

    shapes : add arrow2 and axes

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ export { Vector2, V2, from_degree } from './linear_algebra.js';
 
 export { draw_to_svg } from './draw_svg.js';
 
-export { rectangle, square, arrow } from './shapes.js'
+export { rectangle, square, arrow, arrow2, axes } from './shapes.js'
--- src/shapes.ts
+++ src/shapes.ts
@@ -25,10 +25,43 @@ export function rectangle(width : number, height : number) : Diagram {
 export function square(side : number) : Diagram {
     return rectangle(side, side);
 }
+
+
+/**
+ * Create arrow from start to end
+ * @param start start point of the arrow
+ * @param end end point of the arrow
+ * @param headsize size of the arrow head
+ * @returns a Diagram object
+ */
 export function arrow(start : Vector2, end : Vector2, headsize : number = 3) : Diagram {
     let line_diagram = line(start, end);
     let direction    = end.sub(start);
     let raw_triangle = polygon([V2(0,0), V2(-headsize, headsize/2), V2(-headsize, -headsize/2)]);
-    let head_triangle = raw_triangle.rotate(direction.angle()).translate(direction);
+    let head_triangle = raw_triangle.rotate(direction.angle()).position(end);
     return diagram_combine([line_diagram, head_triangle]);
+}
+
+export function arrow2(start : Vector2, end : Vector2, headsize : number = 3) : Diagram {
+    let line_diagram = line(start, end);
+    let direction    = end.sub(start);
+    let raw_triangle = polygon([V2(0,0), V2(-headsize, headsize/2), V2(-headsize, -headsize/2)]);
+    let head_triangle  = raw_triangle.rotate(direction.angle()).position(end);
+    let head_triangle2 = raw_triangle.rotate(direction.angle()+Math.PI).position(start);
+    return diagram_combine([line_diagram, head_triangle, head_triangle2]);
+}
+
+/**
+ * Draw xy axes
+ * @param xmin minimum x value
+ * @param xmax maximum x value
+ * @param ymin minimum y value
+ * @param ymax maximum y value
+ * @returns a Diagram object
+ */
+export function axes(xmin : number = -50, xmax : number = 50, ymin : number = -50, ymax : number = 50) : Diagram {
+    let xaxis = arrow2(V2(xmin,0), V2(xmax,0));
+    let yaxis = arrow2(V2(0,ymin), V2(0,ymax));
+    return diagram_combine([xaxis, yaxis]).stroke('gray').fill('gray');
+    // return xaxis;
 }
