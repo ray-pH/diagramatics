634cc3e5b2a8adbc4fc098429b5708d457bd8276
Rayhan Alghiffari Azizi <rayhan.azizi9@gmail.com>
2023-09-07 10:44:55 +0700

    shapes : add documentation for rectangle() and square()

--- src/shapes.ts
+++ src/shapes.ts
@@ -3,6 +3,12 @@ import { Vector2, V2 } from './linear_algebra.js';
 
 // function helpers to create common shapes
 
+/**
+ * Create rectange centered at origin
+ * @param width width of the rectangle
+ * @param height height of the rectangle
+ * @returns a Diagram object
+ */
 export function rectangle(width : number, height : number) : Diagram {
     let points = [
         V2(-width/2,-height/2), V2(-width/2, height/2), 
@@ -11,7 +17,11 @@ export function rectangle(width : number, height : number) : Diagram {
     return polygon(points);
 }
 
+/**
+ * Create square centered at origin
+ * @param side side length of the square
+ * @returns a Diagram object
+ */
 export function square(side : number) : Diagram {
     return rectangle(side, side);
 }
-
