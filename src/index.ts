5a1e8b280b9fa8d89144466d4e8034990292e704
Rayhan Alghiffari Azizi <rayhan.azizi9@gmail.com>
2023-09-07 23:18:22 +0700

    shape_graph : add axes_options, plot

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-export { Diagram, polygon, line } from './diagram.js';
+export { Diagram, polygon, line, curve } from './diagram.js';
 
-export { Vector2, V2, from_degree } from './linear_algebra.js';
+export { Vector2, V2, from_degree, linspace } from './linear_algebra.js';
 
 export { draw_to_svg } from './draw_svg.js';
 
-export { rectangle, square, arrow, arrow2, axes } from './shapes.js'
+export { rectangle, square, arrow, arrow2 } from './shapes.js'
+export { axes_empty, plotv, plot } from './shape_graph.js'
--- src/shape_graph.ts
+++ src/shape_graph.ts
@@ -1,24 +1,113 @@
-import { Diagram, polygon, line, diagram_combine } from './diagram.js';
+import { Diagram, polygon, line, curve, diagram_combine } from './diagram.js';
 import { Vector2, V2, linspace } from './linear_algebra.js';
 import { arrow2 } from './shapes.js'
 
 /**
- * Draw xy axes
- * @param xmin minimum x value
- * @param xmax maximum x value
- * @param ymin minimum y value
- * @param ymax maximum y value
+ * Options for axes
+ * Since axes, plot, etc. are separate objects.
+ * Axes options is used so that it's easier to have consistent
+ * setting for multiple objects.
+ */
+export type axes_options = {
+    bbox    : [Vector2, Vector2],
+    xrange  : [number, number],
+    yrange  : [number, number],
+    xticks? : number[],
+    yticks? : number[],
+}
+
+export let default_axes_options : axes_options = {
+    bbox : [V2(-100,-100), V2(100,100)],
+    xrange : [-2, 2],
+    yrange : [-2, 2],
+    xticks : undefined,
+    yticks : undefined,
+}
+
+/**
+ * Draw xy axes without ticks
+ * @param axes_options options for the axes
+ * example: opt = {
+ *    bbox   : [V2(-100,-100), V2(100,100)],
+ * }
  * @returns a Diagram object
  */
-export function axes(xmin : number = -50, xmax : number = 50, ymin : number = -50, ymax : number = 50) : Diagram {
-    let xaxis = arrow2(V2(xmin,0), V2(xmax,0));
-    let yaxis = arrow2(V2(0,ymin), V2(0,ymax));
+export function axes_empty(axes_options? : axes_options) : Diagram {
+    let opt = {...default_axes_options, ...axes_options}; // use default if not defined
+
+    let [lowerleft, upperright] = opt.bbox;
+
+    let xaxis = arrow2(V2(lowerleft.x,0), V2(upperright.x,0));
+    let yaxis = arrow2(V2(0,lowerleft.y), V2(0,upperright.y));
     return diagram_combine([xaxis, yaxis]).stroke('gray').fill('gray');
     // return xaxis;
 }
 
-function plot(axes : Diagram, xarr : number[], yarr : number[]) : Diagram {
+// TODO : 
+// export function axes(axes_options? : axes_options) : Diagram {
+//     let opt = {...default_axes_options, ...axes_options}; // use default if not defined
+// }
+
+/**
+ * Plot a curve given a list of points
+ * @param data list of points
+ * @param axes_options options for the axes
+ * example: opt = {
+ *  bbox   : [V2(-100,-100), V2(100,100)],
+ *  xrange : [-2, 2],
+ *  yrange : [-2, 2],
+ * }
+ */
+export function plotv(data : Vector2[], axes_options? : axes_options) : Diagram {
+    let opt = {...default_axes_options, ...axes_options}; // use default if not defined
+
+    let [lowerleft, upperright] = opt.bbox;
+    let [xmin, xmax] = opt.xrange;
+    let [ymin, ymax] = opt.yrange;
+
+    // map xdata and ydata to axes coordinates
+    let x = data.map(v => lowerleft.x + (v.x-xmin)/(xmax-xmin)*(upperright.x-lowerleft.x));
+    let y = data.map(v => lowerleft.y + (v.y-ymin)/(ymax-ymin)*(upperright.y-lowerleft.y));
+    let v = x.map((x,i) => V2(x,y[i]));
+
+    // split v into segments that are within the bbox
+    let segments : Vector2[][] = [];
+    let current_segment : Vector2[] = [];
+    for (let i=0; i<v.length; i++) {
+        let p = v[i];
+        let is_inside = (p.x >= lowerleft.x && p.x <= upperright.x && p.y >= lowerleft.y && p.y <= upperright.y);
+        if (!is_inside) {
+            if (current_segment.length > 1) segments.push(current_segment);
+            current_segment = [];
+        } else {
+            current_segment.push(p);
+        }
+    }
+    if (current_segment.length > 1) segments.push(current_segment);
+
+    // create separate paths for each segment
+    let paths = segments.map(segment => curve(segment));
+    return diagram_combine(paths).stroke('black').fill('none');
 }
+
+/**
+ * Plot a curve given xdata and ydata
+ * @param xdata x coordinates of the data
+ * @param ydata y coordinates of the data
+ * @param axes_options options for the axes
+ * example: opt = {
+ *   bbox   : [V2(-100,-100), V2(100,100)],
+ *   xrange : [-2, 2],
+ *   yrange : [-2, 2],
+ * }
+ */
+export function plot(xdata : number[], ydata : number[], axes_options? : axes_options) : Diagram {
+    if (xdata.length != ydata.length) throw new Error('xdata and ydata must have the same length');
+    let vdata = xdata.map((x,i) => V2(x,ydata[i]));
+    return plotv(vdata, axes_options);
+}
+
+
 // export function plot_function(f : (x:number)=>number, 
 //     xmin : number =-50, xmax : number = 50, n : number = 100,
 //     yboundmin : number = -50, yboundmax : number = 50
