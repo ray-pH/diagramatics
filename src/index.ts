3f07a6128a563b6a41f562fb109525500aae3009
Rayhan Alghiffari Azizi <rayhan.azizi9@gmail.com>
2023-09-10 00:39:21 +0700

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,9 @@ export { Vector2, V2, Vdir, from_degree, linspace } from './linear_algebra.js';
 export { draw_to_svg } from './draw_svg.js';
 
 export { rectangle, square, regular_polygon, circle, arrow, arrow2 } from './shapes.js'
-export { axes_empty, plot, plotv, plotf, under_curvef } from './shapes/shapes_graph.js'
 
 export { Interactive } from './interactive.js'
 
 // Extra Shapes
+export { axes_empty, plot, plotv, plotf, under_curvef } from './shapes/shapes_graph.js'
 export { inclined_plane } from './shapes/shapes_mechanics.js'
