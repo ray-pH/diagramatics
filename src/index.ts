c005fc6e6b17981a2a11b3be338b5b1883620f6f
Rayhan Alghiffari Azizi <rayhan.azizi9@gmail.com>
2023-09-12 13:37:17 +0700

    index : formatting

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,35 @@
-export { Diagram, polygon, line, curve, empty, text, diagram_combine } from './diagram.js';
+export { 
+    Diagram, polygon, line, curve, empty, text, diagram_combine,
+} from './diagram.js';
 
-export { Vector2, V2, Vdir, from_degree, linspace } from './linear_algebra.js';
+export { 
+    Vector2, V2, Vdir, from_degree, linspace,
+} from './linear_algebra.js';
 
-export { draw_to_svg } from './draw_svg.js';
+export {
+    draw_to_svg,
+} from './draw_svg.js';
 
-export { rectangle, square, regular_polygon, circle, arrow, arrow2, textvar } from './shapes.js'
+export { 
+    rectangle, square, regular_polygon, circle, arrow, arrow2, textvar,
+} from './shapes.js'
 
-export { str_to_mathematical_italic } from './unicode_utils.js'
+export {
+    str_to_mathematical_italic,
+} from './unicode_utils.js'
 
-export { Interactive } from './interactive.js'
+export {
+    Interactive,
+} from './interactive.js'
 
 // Extra Shapes
-export { axes_transform, ax, axes_empty, 
+export { 
+    axes_transform, ax, axes_empty, 
     xtickmark_empty, xtickmark, xticks,
     ytickmark_empty, ytickmark, yticks,
     xyaxes, xygrid,
-    plot, plotv, plotf, under_curvef } from './shapes/shapes_graph.js'
-export { inclined_plane } from './shapes/shapes_mechanics.js'
+    plot, plotv, plotf, under_curvef,
+} from './shapes/shapes_graph.js'
+export { 
+    inclined_plane,
+} from './shapes/shapes_mechanics.js'
