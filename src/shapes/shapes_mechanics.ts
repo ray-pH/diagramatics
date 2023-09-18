2583a6a17468b0e26194c531b91a9c98bb11d043
Rayhan Alghiffari Azizi <rayhan.azizi9@gmail.com>
2023-09-18 21:47:19 +0700

    shapes_mechanics : flip spring() argument order

--- src/shapes/shapes_mechanics.ts
+++ src/shapes/shapes_mechanics.ts
@@ -1,6 +1,12 @@
 import { Diagram, polygon, line, diagram_combine, curve } from '../diagram.js';
 import { Vector2, V2, linspace } from '../linear_algebra.js';
 
+/**
+ * Create an inclined plane.
+ * @param length The length of the inclined plane.
+ * @param angle The angle of the inclined plane.
+ * @returns A diagram of the inclined plane.
+ */
 export function inclined_plane(length : number, angle : number) : Diagram {
     return polygon([V2(0,0), V2(length, length*Math.tan(angle)), V2(length,0)]);
 }
@@ -11,13 +17,13 @@ export function inclined_plane(length : number, angle : number) : Diagram {
  * @param p2 The second point.
  * @param radius The radius of the spring.
  * @param coil_number The number of coils in the spring.
- * @param sample_number The number of points to sample in the spring.
  * @param separation_coefficient The coefficient of separation between coils.
  * \* at 0, no coils are overlapping. (there is no max value)
+ * @param sample_number The number of points to sample in the spring.
  * @returns A diagram of the spring.
  */
 export function spring(p1 : Vector2, p2 : Vector2, radius : number = 1, coil_number : number = 10,
-    sample_number : number = 100, separation_coefficient : number = 0.5) : Diagram {
+    separation_coefficient : number = 0.5, sample_number : number = 100) : Diagram {
 
     // I got this equation from https://www.reddit.com/r/desmos/comments/i3m3yd/interactive_spring_graphic/
 
