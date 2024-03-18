c77493ff96dc61036772957f558de290914bad81
ray-pH <rayhan.azizi9@gmail.com>
2024-03-18 17:58:38 +0700

    shapes_tree: add docs

--- src/shapes/shapes_tree.ts
+++ src/shapes/shapes_tree.ts
@@ -7,15 +7,32 @@ export interface TreeNode {
     children?: TreeNode[];
 }
 
+/**
+ * Create a tree diagram from a tree node
+ * @param node root node of the tree
+ * @param vertical_dist vertical distance between nodes
+ * @param horizontal_gap horizontal gap between nodes
+ * @returns tree diagram
+ */
 export function tree(node : TreeNode, vertical_dist : number, horizontal_gap : number) : Diagram {
     let treeDraw = calculate_tree_buchheim(node, vertical_dist, horizontal_gap);
     return diagram_from_treeDraw(treeDraw);
 }
 
+/**
+ * Mirror a tree node
+ * @param node root node of the tree
+ * @returns mirrored tree node
+ */
 export function mirror_treenode(node : TreeNode) : TreeNode {
     return {value: node.value, children: (node.children ?? []).map(mirror_treenode).reverse()};
 }
 
+/**
+ * Helper function to create a diagram from a treeDraw
+ * @param node treeDraw node
+ * @returns diagram
+ */
 function diagram_from_treeDraw(node : TreeDraw) : Diagram {
     let node_dg = node.diagram;
     let children_dglist = node.children.map(diagram_from_treeDraw);
